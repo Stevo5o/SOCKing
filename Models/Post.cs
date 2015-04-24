5b61d2a1e9dcd5197569cdfa7a1c61c21f899c66
Stevo5o <stevo.joc@gmail.com>
2015-04-24 09:18:18 +0100

    Admin Posts

---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOCKing.Models
+{
+    class Post
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Published { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
